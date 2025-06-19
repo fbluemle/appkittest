32298935698e89f4203699e932a2da9d7fdb8cd0
Frieder Bluemle <frieder.bluemle@gmail.com>
2025-06-19 16:14:22 -0700

    Integrate AppKit and Wagmi

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 
 [1]: https://github.com/fbluemle/appkittest/workflows/ci/badge.svg
 [2]: https://github.com/fbluemle/appkittest/actions
+
+Get Project ID at https://cloud.reown.com/
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,57 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import '@walletconnect/react-native-compat';
 
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+import {
+  AppKit,
+  AppKitButton,
+  createAppKit,
+  defaultWagmiConfig,
+} from '@reown/appkit-wagmi-react-native';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {WagmiProvider} from 'wagmi';
+import {mainnet} from 'wagmi/chains';
+
+// Get Project ID from https://cloud.reown.com/
+const projectId = '';
+
+const queryClient = new QueryClient();
+
+const metadata = {
+  name: 'appkittest',
+  description: 'appkittest',
+  url: 'https://reown.com/appkit',
+  icons: ['https://avatars.githubusercontent.com/u/179229932'],
+  redirect: {
+    native: 'appkittest://',
+  },
+};
+
+const wagmiConfig = defaultWagmiConfig({
+  chains: [mainnet],
+  projectId,
+  metadata,
+});
+
+createAppKit({
+  projectId,
+  wagmiConfig,
+});
 
 const App = () => {
   return (
-    <SafeAreaProvider>
-      <View style={styles.container}>
-        <Text>appkittest</Text>
-      </View>
-    </SafeAreaProvider>
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <SafeAreaProvider>
+          <View style={styles.container}>
+            <AppKitButton balance="show" />
+            <AppKit />
+          </View>
+        </SafeAreaProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 };
 
