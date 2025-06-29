bc4ded6cd108d2370035e25bf57ac449d03ce49d
Frieder Bluemle <frieder.bluemle@gmail.com>
2025-06-29 16:29:25 -0700

    Add react-native-gesture-handler

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'react-native',
+  setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?|@walletconnect|@wagmi|react-native-animatable|react-native-modal|react-native-safe-area-context|react-native-url-polyfill|wagmi)/)',
