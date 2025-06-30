819447d78fb37efd7bd6585266b2292c40aca0d3
Frieder Bluemle <frieder.bluemle@gmail.com>
2025-06-30 16:58:23 -0700

    Add AppKit dependencies

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   preset: 'react-native',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|react-native-animatable|react-native-modal|react-native-safe-area-context|react-native-url-polyfill)/)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|@walletconnect|@wagmi|react-native-animatable|react-native-modal|react-native-safe-area-context|react-native-url-polyfill|wagmi)/)',
   ],
 };
